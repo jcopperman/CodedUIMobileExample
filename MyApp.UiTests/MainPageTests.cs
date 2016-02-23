9de48d02a538267eeb898f49a994a9a614b43e15
jcopperman <opperman.jonathan@gmail.com>
2016-02-23 11:50:06 +0200

    Initial

---
+++ MyApp.UiTests/MainPageTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Input;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+using Microsoft.VisualStudio.TestTools.UITesting.DirectUIControls;
+using Microsoft.VisualStudio.TestTools.UITesting.WindowsRuntimeControls;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace MyApp.UiTests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest(CodedUITestType.WindowsStore)]
+    public class MainPageTests
+    {
+        public MainPageTests()
+        {
+        }
+
+        [TestMethod]
+        public void MyApp_WhenLaunched_TextIsBlockDisplayed()
+        {
+            XamlWindow myAppWindow = XamlWindow.Launch("2f97ff00-c4d4-40e2-bb45-4fa1a9a867b8_b3r19t9dwpk1r!App");
+         
+            var textBlockText = this.UIMap.UIMyAppWindow.UITextBlockText.DisplayText;
+
+            Assert.AreEqual("TextBlock", textBlockText);
+        }
+
+        [TestMethod]
+        public void MessageDialog_WhenButtonClicked_ButtonTextIsYes()
+        {
+            XamlWindow myAppWindow = XamlWindow.Launch("2f97ff00-c4d4-40e2-bb45-4fa1a9a867b8_b3r19t9dwpk1r!App");
+
+            var btn = this.UIMap.UIMyAppWindow.UIButtonButton;          
+
+            Gesture.Tap(btn);
+
+            var messagedialog = this.UIMap.UILoremIpsumWindow.Name;
+
+            Assert.AreEqual("Lorem Ipsum", messagedialog);         
+        }
+
+        [TestMethod]
+        public void MessageDialog_WhenButtonClicked_ShowPopupTextDisplaysYes()
+        {
+            XamlWindow myAppWindow = XamlWindow.Launch("2f97ff00-c4d4-40e2-bb45-4fa1a9a867b8_b3r19t9dwpk1r!App");
+
+            var btn = this.UIMap.UIMyAppWindow.UIButtonButton;
+            
+            Gesture.Tap(btn);
+
+            var yesBtn = this.UIMap.UILoremIpsumWindow.UIButtonBarToolBar.UIYesButton;
+
+            Gesture.Tap(yesBtn);
+
+            var messagedialog = this.UIMap.UILoremIpsumWindow.Name;
+
+            Assert.AreEqual("Result: Yes (0)", btn.DisplayText);
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
---
+++ MyApp/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MyApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Windows.UI.Popups.MessageDialog(
+                "Aliquam laoreet magna sit amet mauris iaculis ornare. " +
+                "Morbi iaculis augue vel elementum volutpat.",
+                "Lorem Ipsum");
+
+            dialog.Commands.Add(new Windows.UI.Popups.UICommand("Yes") { Id = 0 });
+            dialog.Commands.Add(new Windows.UI.Popups.UICommand("No") { Id = 1 });
+
+            if (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily != "Windows.Mobile")
+            {
+                // Adding a 3rd command will crash the app when running on Mobile 
+                dialog.Commands.Add(new Windows.UI.Popups.UICommand("Maybe later") { Id = 2 });
+            }
+
+            dialog.DefaultCommandIndex = 0;
+            dialog.CancelCommandIndex = 1;
+
+            var result = await dialog.ShowAsync();
+
+            var btn = sender as Button;
+            btn.Content = $"Result: {result.Label} ({result.Id})";
+
+        }
+    }
+}
